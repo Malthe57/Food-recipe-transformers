e87035b1bfa16afa7a2dc9654e56eb3b20570211
Malthe Andersen <s194257@student.dtu.dk>
2023-10-10 17:11:09 +0200

    Added dataset_class
    Also a scipt for loading an image from the dataset

---
+++ dataset_class.py
@@ -0,0 +1,40 @@
+# Imports
+import os
+import pandas as pd
+
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class FoodRecipeDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_text = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_text)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_text.iloc[idx, 4] + ".jpg")
+        image = Image.open(img_path)
+        title = self.img_text.iloc[idx, 1]
+        ingredients = self.img_text.iloc[idx, 2]
+        instructions = self.img_text.iloc[idx, 3]
+        cleaned_ingredients = self.img_text.iloc[idx, 5]
+
+
+
+
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            title = self.target_transform(title)
+            ingredients = self.target_transform(ingredients)
+            instructions = self.target_transform(instructions)
+            cleaned_ingredients = self.target_transform(cleaned_ingredients)
+
+
+        return image, title, ingredients, instructions, cleaned_ingredients
---
+++ test_datasetclass.py
@@ -0,0 +1,27 @@
+
+import os
+import torch
+from torch.utils.data import DataLoader, random_split
+from dataset_class import FoodRecipeDataset
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+current_working_directory = os.getcwd()
+images_path = os.path.join(current_working_directory, "dataset/Food Images")
+text_path = os.path.join(current_working_directory, "dataset/Food Ingredients and Recipe Dataset with Image Name Mapping.csv")
+
+FoodRecipeData = FoodRecipeDataset(text_path, images_path, transforms.ToTensor())
+
+
+training_share = 0.6 #Proportion of data that is alotted to the training set
+training_size = int(training_share*len(FoodRecipeData))
+test_size = len(FoodRecipeData) - training_size
+generator = torch.Generator().manual_seed(42)
+training_data, test_data = random_split(FoodRecipeData, [training_size, test_size], generator)
+
+train_dataloader = DataLoader(training_data, batch_size=32, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=32, shuffle=True)
+
+image, title, ingredients, instructions, cleaned_ingredients = next(iter(train_dataloader))
+plt.imshow(image[0].permute(1,2,0))
+plt.show()
