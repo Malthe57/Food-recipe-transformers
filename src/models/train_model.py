82df191740fa25458d74b5d636db13f12fd04212
TECH-yufu <yuch0001@gmail.com>
2023-11-07 14:28:58 +0100

    Update train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -17,7 +17,7 @@ import sys
 sys.path.append("../Food-recipe-transformers/src/")
 from utils.loss import TripletLoss
 
-from VocabImagedataset_class import VocabImageDataset
+from VocabImagedataset_class import VocabImageDataset, pad_input, collate_batch
 from image_encoder import ImageEncoder
 from text_encoder import TextEncoder
 from joint_encoder import JointEmbedding
@@ -40,7 +40,7 @@ def prepare_dataloaders(batch_size):
     tokenizer = get_tokenizer("basic_english")
     transform = transforms.Compose([transforms.Resize((64,64)),transforms.ToTensor()])
 
-    VocabImageData = VocabImageDataset(text_path, images_path, vocab, tokenizer, transform)
+    VocabImageData = VocabImageDataset(annotations_file=text_path, img_dir=images_path, vocab=vocab, tokenizer=tokenizer, device=device, transform=transform)
 
 
     training_share = 0.6 #Proportion of data that is alotted to the training set
@@ -49,13 +49,13 @@ def prepare_dataloaders(batch_size):
     generator = torch.Generator().manual_seed(42)
     training_data, test_data = random_split(VocabImageData, [training_size, test_size], generator)
 
-    trainloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
-    testloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+    trainloader = DataLoader(training_data, batch_size=batch_size, shuffle=True, collate_fn=collate_batch)
+    testloader = DataLoader(test_data, batch_size=batch_size, shuffle=True, collate_fn=collate_batch)
 
     return trainloader, testloader, training_data, test_data
 
 
-def main(image_size=(32,32), patch_size=(4,4), channels=3, 
+def main(image_size=(64,64), patch_size=(8,8), channels=3, 
          embed_dim=128, num_heads=4, num_layers=4, num_classes=2,
          pos_enc='learnable', pool='cls', dropout=0.3, fc_dim=None, 
          num_epochs=20, batch_size=16, lr=1e-4, warmup_steps=625,
@@ -67,14 +67,15 @@ def main(image_size=(32,32), patch_size=(4,4), channels=3,
 
     train_iter, test_iter, _, _ = prepare_dataloaders(batch_size=batch_size)
 
-    model = JointEmbedding(
-        ImageEncoder(image_size=image_size, patch_size=patch_size, channels=channels, 
+
+    image_encoder = ImageEncoder(image_size=image_size, patch_size=patch_size, channels=channels, 
                 embed_dim=embed_dim, num_heads=num_heads, num_layers=num_layers,
                 pos_enc=pos_enc, pool=pool, dropout=dropout, fc_dim=fc_dim, 
-                num_classes=num_classes), 
-        TextEncoder(embed_dim=embed_dim, num_heads=num_heads, num_layers = num_layers, max_seq_len = 512,
-                   dropout = dropout, fc_dim = fc_dim, num_tokens = 50000, pool = "mean", pos_enc = pos_enc),
-        embed_dim = embed_dim)
+                num_classes=num_classes)
+    
+    text_encoder = TextEncoder(embed_dim=embed_dim, num_heads=num_heads, num_layers = num_layers, max_seq_len = 512,
+                   dropout = dropout, fc_dim = fc_dim, num_tokens = 50000, pool = "mean", pos_enc = pos_enc)
+    model = JointEmbedding(image_encoder=image_encoder, text_encoder=text_encoder, embed_dim = embed_dim)
 
 
     if torch.cuda.is_available():
@@ -86,6 +87,9 @@ def main(image_size=(32,32), patch_size=(4,4), channels=3,
     # training loop
     for e in range(num_epochs):
         print(f'\n epoch {e}')
+
+        batch_loss = []
+
         model.train()
         for image, title, ingredients, instructions, cleaned_ingredients in tqdm.tqdm(train_iter):
             if torch.cuda.is_available():
@@ -93,12 +97,17 @@ def main(image_size=(32,32), patch_size=(4,4), channels=3,
             opt.zero_grad()
             image_features, text_features = model(image, title, ingredients, instructions)
             loss = loss_function(image_features, text_features)
+
+            batch_loss.append(loss.item())
+
             loss.backward()
             # if the total gradient vector has a length > 1, we clip it back down to 1.
             if gradient_clipping > 0.0:
                 nn.utils.clip_grad_norm_(model.parameters(), gradient_clipping)
             opt.step()
             sch.step()
+
+        print(f'-- {"train"} loss {np.mean(batch_loss):.3}')
 
         # with torch.no_grad():
         #     model.eval()
@@ -118,5 +127,5 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  
     print(f"Model will run on {device}")
     set_seed(seed=1)
-    model = main()
+    model = main(image_size=(64, 64), patch_size=(8,8))
     torch.save(model, 'first_test_model.pt')
