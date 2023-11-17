9059c8ea8edb267dfa064b7a302cbc487ab227f7
TECH-yufu <yuch0001@gmail.com>
2023-11-17 11:45:24 +0100

    Update playground.py

--- src/playground.py
+++ src/playground.py
@@ -8,6 +8,13 @@ loss_function = TripletLoss()
 a = np.array([[1.1, 2.2,], [2.2, 3.3], [3.3, 4.4]])
 b = np.array([[2.2, 3.3], [3.3, 4.4], [4.4, 5.5]])
 
+loss = loss_function(torch.tensor(a), torch.tensor(b))  
+
+# computing the loss in hand
+# we get image loss (cost_im in loss.py)
+# [[0, 0.0915, 0.0855]; [0.1015, 0, 0.0977]; [0.0989, 0.1005, 0]]
+# and recipe loss (cost_s in loss.py)
+# [[0, 0.854, 0.078]; [0.1077, 0, 0.0967], [0.1061, 0.1015, 0]]
 
 print(a)
 
