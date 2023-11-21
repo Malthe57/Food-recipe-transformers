2b3647ac2c1dd19090dc159a1312c70e321fe860
TECH-yufu <yuch0001@gmail.com>
2023-11-21 15:53:05 +0100

    clip

--- src/clip_model.py
+++ src/clip_model.py
@@ -1,56 +1,75 @@
 import torch
 import clip
 from PIL import Image
+from CLIPDataset_class import ClipFoodRecipeDataset
 from VocabImagedataset_class import VocabImageDataset, pad_input, collate_batch, collate_clip
 import torchvision.transforms as transforms
 from torchtext.data.utils import get_tokenizer
 import os
 from torch.utils.data import DataLoader, random_split
-
-def prepare_dataloaders(batch_size):
-    
-    current_working_directory = os.getcwd()
-    images_path = os.path.join(current_working_directory, "src/dataset/Food Images")
-    text_path = os.path.join(current_working_directory, "src/dataset/food.csv")
-    vocab = torch.load("src/dataset/vocab.pt")
-    tokenizer = get_tokenizer("basic_english")
-    transform = transforms.Compose([transforms.Resize((64,64)),transforms.ToTensor()])
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    VocabImageData = VocabImageDataset(annotations_file=text_path, img_dir=images_path, vocab=vocab, tokenizer=tokenizer, device=device, transform=transform, vocabularise=False)
+import numpy as np  
+import pickle
 
 
-    training_share = 0.8 #Proportion of data that is alotted to the training set
-    training_size = int(training_share*len(VocabImageData))
-    test_size = len(VocabImageData) - training_size
-    generator = torch.Generator().manual_seed(42)
-    training_data, temp = random_split(VocabImageData, [training_size, test_size], generator)
-    test_data, val_data = random_split(temp, [int(0.4*len(temp)), int(0.6*len(temp))], generator)
+current_working_directory = os.getcwd()
+images_path = os.path.join(current_working_directory, "src/dataset/Food Images")
+text_path = os.path.join(current_working_directory, "src/dataset/food.csv")
 
-    trainloader = DataLoader(training_data, batch_size=batch_size, shuffle=True, collate_fn=collate_clip)
-    valloader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
-    testloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+VocabImage = ClipFoodRecipeDataset(text_path, images_path, transform=None)
+
+training_share = 0.8 #Proportion of data that is alotted to the training set
+training_size = int(training_share*len(VocabImage))
+test_size = len(VocabImage) - training_size
+generator = torch.Generator().manual_seed(42)
+training_data, temp = random_split(VocabImage, [training_size, test_size], generator)
+test_data, val_data = random_split(temp, [int(0.4*len(temp)), int(0.6*len(temp))], generator)
+
+def collate_clip(batch):
+
+    image, title, ingredients, instructions, cleaned_ingredients, id = zip(*batch)
+
+    image_tensor = [img for img in [transforms.ToPILImage(mode='RGB')(img) for img in image]]
+    title_tensor = title
+    ingredients_tensor = ingredients
+    instructions_tensor = instructions
+    cleaned_ingredients_tensor = cleaned_ingredients
+
+    return image_tensor, title_tensor, ingredients_tensor, instructions_tensor, cleaned_ingredients_tensor, id[0] # get id of tuple
+
+def dump_pickles(features1, features2, ids, pkl_file_1="models/test_img_features.pkl", pkl_file_2="models/test_text_features.pkl"):
+    with open(pkl_file_1, 'wb') as f:
+        pickle.dump((features1, ids), f)
+    with open(pkl_file_2, 'wb') as f:
+        pickle.dump((features2, ids), f)
+
+def inference(pkl_file_1="models/test_img_features.pkl", pkl_file_2="models/test_img_features.pkl"):
+
+    testloader = DataLoader(test_data, batch_size=1, shuffle=False, collate_fn=collate_clip)
+
+    # device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = 'cpu'
+    model, preprocess = clip.load("ViT-B/32", device=device)
 
 
-    return trainloader, valloader, testloader, training_data, val_data, test_data
+    test_img_features = []
+    test_text_features = []
+    ids = []
 
-trainloader, valloader, testloader, training_data, val_data, test_data = prepare_dataloaders(batch_size=32)
+    for image, title, ingredients, instructions, cleaned_ingredients, idx in testloader:
 
-for image, title, ingredients, instructions, cleaned_ingredients, _ in trainloader:
-    break
+        image_input =  torch.stack([preprocess(img) for img in image])
+        text_inputs = clip.tokenize(["".join(t) for t in title])
+        
+        with torch.no_grad():
+            image_features = model.encode_image(image_input)
+            text_features = model.encode_text(text_inputs)
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load("ViT-B/32", device=device)
+            ids.append(idx)
 
-image = preprocess(Image.open("src/dataset/Food Images/3-ingredient-fudge-pops.jpg")).unsqueeze(0).to(device)
-text = clip.tokenize(["an icecream", "a dog", "a cat"]).to(device)
+            test_img_features.append(image_features.detach().cpu().numpy()[0])
+            test_text_features.append(text_features.detach().cpu().numpy()[0])
 
-with torch.no_grad():
-    image_features = model.encode_image(image)
-    text_features = model.encode_text(text)
-    
-    logits_per_image, logits_per_text = model(image, text)
-    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+            dump_pickles(features1=np.asarray(test_img_features), features2=np.asarray(test_text_features), ids=np.array(ids), pkl_file_1=pkl_file_1, pkl_file_2=pkl_file_2)
 
-print("Label probs:", probs) 
+if __name__ == "__main__":
+    inference(pkl_file_1=f"models/test_img_features_clip.pkl", pkl_file_2=f"models/test_text_features_clip.pkl")
--- src/dataset_class.py
+++ src/dataset_class.py
@@ -5,6 +5,9 @@ import pandas as pd
 import torch
 from PIL import Image
 from torch.utils.data import Dataset
+from torchvision import transforms
+from torch.utils.data import DataLoader, random_split
+
 
 
 class FoodRecipeDataset(Dataset):
@@ -27,8 +30,6 @@ class FoodRecipeDataset(Dataset):
         cleaned_ingredients = str(self.img_text.iloc[idx, 4])
 
 
-
-
         if self.transform:
             rgb_im = self.transform(rgb_im)
         if self.target_transform:
@@ -39,4 +40,62 @@ class FoodRecipeDataset(Dataset):
 
 
         return rgb_im, title[:512], ingredients[:512], instructions[:512], cleaned_ingredients[:512], idx
+    
+class ApplyTransforms(Dataset):
+    def __init__(self, dataset, split):
+        self.dataset = dataset
+        self.split = split
+        self.train_transforms = transforms.Compose([transforms.Resize((256,256)), 
+                                                    transforms.RandomHorizontalFlip(),
+                                                    transforms.RandomCrop(224),
+                                                    transforms.ToTensor(),
+                                    transforms.Normalize((0.485, 0.456, 0.406),
+                                                (0.229, 0.224, 0.225))])
+        
+        self.test_transforms = transforms.Compose([transforms.Resize((256,256)), transforms.CenterCrop(224),
+                                                    transforms.ToTensor(),
+                                    transforms.Normalize((0.485, 0.456, 0.406),
+                                                (0.229, 0.224, 0.225))])
 
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        rgb_im, title, ingredients, instructions, cleaned_ingredients, idx = self.dataset[idx]
+        if self.split == 'train':
+            rgb_im = self.train_transforms(rgb_im)
+            # print("Applying train transforms: RandomCrop")
+        elif self.split == 'val':
+            rgb_im = self.test_transforms(rgb_im)
+            # print("Applying val transforms: CenterCrop")
+        elif self.split == 'test':
+            rgb_im = self.test_transforms(rgb_im)
+            # print("Applying test transforms: CenterCrop")
+
+        return rgb_im, title, ingredients, instructions, cleaned_ingredients, idx
+
+
+if __name__ == "__main__":
+    batch_size = 32
+
+    current_working_directory = os.getcwd()
+    images_path = os.path.join(current_working_directory, "src/dataset/Food Images")
+    text_path = os.path.join(current_working_directory, "src/dataset/food.csv")
+
+    VocabImage = FoodRecipeDataset(text_path, images_path, transform=None)
+
+    training_share = 0.8 #Proportion of data that is alotted to the training set
+    training_size = int(training_share*len(VocabImage))
+    test_size = len(VocabImage) - training_size
+    generator = torch.Generator().manual_seed(42)
+    training_data, temp = random_split(VocabImage, [training_size, test_size], generator)
+    test_data, val_data = random_split(temp, [int(0.4*len(temp)), int(0.6*len(temp))], generator)
+
+    training_data = ApplyTransforms(training_data, split='train')
+    val_data = ApplyTransforms(val_data, split='val')
+    test_data = ApplyTransforms(test_data, split='test')
+
+    trainloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+
+    valloader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
